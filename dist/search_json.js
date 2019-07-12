50c51bc2072b4a29709f8b55d13b4bcb859eb5c8
苏文雄 <suwenxiong@meituan.com>
2019-07-12 17:22:14 +0800

    feat: docs

--- dist/search_json.js
+++ dist/search_json.js
@@ -2,7 +2,7 @@ window.ydoc_plugin_search_json = {
   "sean 个人空间": [
     {
       "title": "个人介绍",
-      "content": "网名 Sean，90后，前端工程师邮箱：hellosean1025@163.com",
+      "content": "网名 Sean，前端工程师邮箱：hellosean1025@163.com",
       "url": "/index.html",
       "children": [
         {
