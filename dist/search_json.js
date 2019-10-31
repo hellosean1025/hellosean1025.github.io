690ebcaf30d6dc1bbdff8ab4c9943499745914fa
苏文雄 <suwenxiong@meituan.com>
2019-10-31 16:54:47 +0800

    feat: docs

--- dist/search_json.js
+++ dist/search_json.js
@@ -8,7 +8,7 @@ window.ydoc_plugin_search_json = {
         {
           "title": "作品",
           "url": "/index.html#作品",
-          "content": "作品yapi  强大的接口管理平台\nydoc  基于 markdown 生成文档站点\nJSF    基于json-schema生成中后台系统表单\n"
+          "content": "作品yapi  强大的接口管理平台\nydoc  基于 markdown 生成文档站点\nJSF    基于json-schema生成中后台系统表单\njson-schema-visual-editor  json-schema 编辑器\n"
         }
       ]
     }
--- docs/index.md
+++ docs/index.md
@@ -9,3 +9,4 @@
 * [yapi](https://hellosean1025.github.io/yapi)  强大的接口管理平台
 * [ydoc](https://hellosean1025.github.io/ydoc)  基于 markdown 生成文档站点
 * [JSF](https://hellosean1025.github.io/jsf)    基于json-schema生成中后台系统表单
+* [json-schema-visual-editor](https://hellosean1025.github.io/json-schema-visual-editor/)  json-schema 编辑器
