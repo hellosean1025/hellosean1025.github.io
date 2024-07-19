7bc077d6db03303a67a8f797f030ad8869c2d8fd
suwenxiong <suwenxiong@bytedance.com>
2024-07-19 12:30:37 +0800

    feat: docs

--- dist/search_json.js
+++ dist/search_json.js
@@ -2,7 +2,7 @@ window.ydoc_plugin_search_json = {
   "sean 个人空间": [
     {
       "title": "个人介绍",
-      "content": "工作状态：目前就职于美团，看机会请联系我邮箱：hellosean1025@163.com",
+      "content": "工作状态：目前就职于字节跳动，是某研发团队负责人团队在北京、广州急招前端、后端[2024.7.19更新]看机会请发邮件给我邮箱：hellosean1025@163.com",
       "url": "/index.html",
       "children": [
         {
